8bf3026b85d4900e9d9be950c64c601025930de3
Hiago <hiagofml11@gmail.com>
2026-03-19 17:45:16 -0300

    feat: melhoramento do Modal

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+
+let isEntrada = null
+
 function abrirModal() {
     document.getElementById('Modal').showModal()
+}
+
+function fecharModal() {
+    document.getElementById('Modal').close()
+}
+
+function btnentry() {
+    isEntrada = true
+    document.getElementById('btn-entrada').classList.add('ativo-entrada')
+    document.getElementById('btn-saida').classList.remove('ativo-saida')
+}
+
+function btnout() {
+    isEntrada = false
+    document.getElementById('btn-saida').classList.add('ativo-saida')
+    document.getElementById('btn-entrada').classList.remove('ativo-entrada')
+}
+
+function btncadastrar() {
+
+    if (isEntrada === null) {
+        alert('Selecione uma opção de entrada ou saída')
+        return
+    }
+
+    if (isEntrada) {
+        alert('Entrada selecionada')
+        document.getElementById('btn-entrada').classList.remove('ativo-entrada')
+    }else {
+        alert('Saída selecionada')
+        document.getElementById('btn-saida').classList.remove('ativo-saida')
+    }
+
+    isEntrada = null
+    fecharModal()
 }
